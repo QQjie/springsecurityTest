030093fe3f1afa9304cf0eac6405c5f65e358f4f
huangjie <17773168061@163.com>
2018-07-24 12:36:33 +0800

    添加PermissionService方法

--- src/main/java/com/cnsunet/kjw/controller/sysmanage/PermissionController.java
+++ src/main/java/com/cnsunet/kjw/controller/sysmanage/PermissionController.java
@@ -101,7 +101,7 @@ public class PermissionController {
      * @Modyfied by
      */
     @RequestMapping(value = "/api/pmsforrole",method = RequestMethod.POST)
-    @ApiOperation(value ="获取当前登录用户的所有权限列表",response = String.class,httpMethod = "POST",notes="获取当前登录用户的所有权限列表")
+    @ApiOperation(value ="给角色划分或修改权限",response = String.class,httpMethod = "POST",notes="给角色划分或修改权限")
     public String addOrUpdatePmsByRoleid(
             @ApiParam(value = "角色id",required = true) @RequestParam(value = "roleId" ,required = true) String roleId,
             @ApiParam(value = "权限id集合用,分开",required = true) @RequestParam(value = "pmsids" ,required = true) String pmsids
@@ -158,5 +158,214 @@ public class PermissionController {
             return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
         }
     }
+    /**
+     * @Author  huangjie
+     * 描述 给权限增加或修改操作权限
+     * HTTP方式 POST
+     * API路径 /api/oprforpms
+     * 方法名 AddOrUpdateOprForPms
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/oprforpms",method = RequestMethod.POST)
+    @ApiOperation(value = "给权限增加或修改操作权限",response = String.class,httpMethod = "POST",notes = "给权限增加或修改操作权限")
+    public String AddOrUpdateOprForPms(
+            @ApiParam(value = "权限Id",required = true)@RequestParam(value = "pmsId",required = true) String pmsId,
+            @ApiParam(value = "操作Id列表，用英文逗号','隔开") @RequestParam(value = "oprIds",required = true) String oprIds
+    ){
+            List<Integer> list=new ArrayList<>();
+            String[] arr=oprIds.split(",");
+            for (int i = 0; i < arr.length; i++) {
+                list.add(Integer.valueOf(arr[i]));
+            }
+            try{
+                int result=permissionService.updatePmsOperate(Integer.valueOf(pmsId),list);
+                return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "给权限增加或修改操作权限成功", result).toString();
+            }catch(DBErrorException e){
+                //抛出异常返回异常信息
+                logger.error("controller:PermissionController. function:AddOrUpdateOprForPms..msg:POST  DBErrorException. error:"+e.getMessage());
+                return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+            }catch (Exception e){
+                //抛出异常返回异常信息
+                logger.error("controller:PermissionController. function:AddOrUpdateOprForPms..msg:POST  Exception. error:"+e.getMessage());
+                return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+            }
+    }
+
+    /**
+     * @Author  huangjie
+     * 描述 新增权限管理信息
+     * HTTP方式  POST
+     * API路径   /api/permission
+     * 方法名  addPermission
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+     @RequestMapping(value = "/api/permission",method = RequestMethod.POST)
+     @ApiOperation(value = "新增权限管理信息",response = String.class,httpMethod = "POST",notes = "新增权限管理信息")
+     public String addPermission(
+             @ApiParam(value = "权限名称",required = true)@RequestParam(value = "permissionName",required = true) String permissionName
+     ){
+         PermissionModel permissionModel=new PermissionModel();
+         permissionModel.setPerssionName(permissionName);
+         try{
+             int result=permissionService.addPermission(permissionModel);
+             return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "新增权限管理信息成功", result).toString();
+         }catch(DBErrorException e){
+             //抛出异常返回异常信息
+             logger.error("controller:PermissionController. function:addPermission..msg:POST  DBErrorException. error:"+e.getMessage());
+             return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+         }catch (Exception e){
+             //抛出异常返回异常信息
+             logger.error("controller:PermissionController. function:addPermission..msg:POST  Exception. error:"+e.getMessage());
+             return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+         }
+     }
+
+    /**
+     * @Author  huangjie
+     * 描述 修改权限管理信息
+     * HTTP方式  PUT
+     * API路径   /api/permission
+     * 方法名  updatePermission
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/permission",method = RequestMethod.PUT)
+    @ApiOperation(value = "修改权限管理信息",response = String.class,httpMethod = "PUT",notes = "修改权限管理信息")
+    public String updatePermission(
+            @ApiParam(value = "权限名称",required = true)@RequestParam(value = "permissionName",required = true) String permissionName
+    ){
+        PermissionModel permissionModel=new PermissionModel();
+        permissionModel.setPerssionName(permissionName);
+        try{
+            int result=permissionService.updatePermission(permissionModel);
+            return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "修改权限管理信息成功", result).toString();
+        }catch(DBErrorException e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:updatePermission..msg:PUT  DBErrorException. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+        }catch (Exception e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:updatePermission..msg:PUT  Exception. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+        }
+    }
+
+    /**
+     * @Author  huangjie
+     * 描述 删除权限管理信息
+     * HTTP方式  POST
+     * API路径   /api/permission
+     * 方法名  deletePermission
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/permission",method = RequestMethod.DELETE)
+    @ApiOperation(value = "删除权限管理信息",response = String.class,httpMethod = "DELETE",notes = "删除权限管理信息")
+    public String deletePermission(
+            @ApiParam(value = "权限Id",required = true)@RequestParam(value = "permissionId",required = true) String permissionId
+    ){
+        try{
+            int result=permissionService.deletePermission(Integer.valueOf(permissionId));
+            return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "删除权限管理信息成功", result).toString();
+        }catch(DBErrorException e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:deletePermission..msg:POST  DBErrorException. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+        }catch (Exception e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:deletePermission..msg:POST  Exception. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+        }
+    }
+
+    /**
+     * @Author  huangjie
+     * 描述 查询所有权限管理信息
+     * HTTP方式  GET
+     * API路径   /api/permission
+     * 方法名  getAllPermission
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/permission",method = RequestMethod.GET)
+    @ApiOperation(value = "查询所有权限管理信息",response = String.class,httpMethod = "GET",notes = "查询所有权限管理信息")
+    public String getAllPermission(
+            @ApiParam(value = "权限名称",required = true)@RequestParam(value = "permissionName",required = true) String permissionName
+    ){
+        List<PermissionModel> result=new ArrayList<>();
+        try{
+            result=permissionService.getAllPermission();
+            return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "新增权限管理信息成功", result).toString();
+        }catch(DBErrorException e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getAllPermission..msg:GET  DBErrorException. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+        }catch (Exception e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getAllPermission..msg:GET  Exception. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+        }
+    }
+
+    /**
+     * @Author  huangjie
+     * 描述 根据id查询所有权限信息
+     * HTTP方式  GET
+     * API路径   /api/permissionById
+     * 方法名  getPermissionById
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/permissionById",method = RequestMethod.GET)
+    @ApiOperation(value = "根据id查询所有权限信息",response = String.class,httpMethod = "GET",notes = "根据id查询所有权限信息")
+    public String getPermissionById(
+            @ApiParam(value = "权限Id",required = true)@RequestParam(value = "permissionId",required = true) String permissionId
+    ){
+        PermissionModel result=null;
+        try{
+            result=permissionService.getPermissionByid(Integer.valueOf(permissionId));
+            return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "新增权限管理信息成功", result).toString();
+        }catch(DBErrorException e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getPermissionById..msg:GET  DBErrorException. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+        }catch (Exception e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getPermissionById..msg:GET  Exception. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+        }
+    }
+
+    /**
+     * @Author  huangjie
+     * 描述 根据Name查询所有权限信息
+     * HTTP方式  GET
+     * API路径   /api/permissionByName
+     * 方法名  getPermissionByName
+     * 方法异常  DBErrorException Exception
+     * @Modyfied by
+     */
+    @RequestMapping(value = "/api/permissionByName",method = RequestMethod.GET)
+    @ApiOperation(value = "根据Name查询所有权限信息",response = String.class,httpMethod = "GET",notes = "根据Name查询所有权限信息")
+    public String getPermissionByName(
+            @ApiParam(value = "权限名称",required = true)@RequestParam(value = "permissionName",required = true) String permissionName
+    ){
+        PermissionModel result=null;
+        try{
+            result=permissionService.getPermissionByName(permissionName);
+            return new JsonResponseData(true, StatusDefineMessage.GetMessage(StatusDefine.SUCCESS), StatusDefine.SUCCESS, "新增权限管理信息成功", result).toString();
+        }catch(DBErrorException e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getPermissionByName..msg:GET  DBErrorException. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.DB_ERROR), StatusDefine.DB_ERROR, "", null).toString();
+        }catch (Exception e){
+            //抛出异常返回异常信息
+            logger.error("controller:PermissionController. function:getPermissionByName..msg:GET  Exception. error:"+e.getMessage());
+            return new JsonResponseData(false, StatusDefineMessage.GetMessage(StatusDefine.SYS_ERROR), StatusDefine.SYS_ERROR, "", null).toString();
+        }
+    }
+
 
 }
